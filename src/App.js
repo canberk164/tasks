3054f5c7bbd007a73e17a73e4a12e9b42a6d307c
canberk.tolunbuke <36010295+canberk164@users.noreply.github.com>
2024-02-17 21:14:30 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import TaskCreate from "./components/TaskCreate";
 import TaskList from "./components/TaskList";
 import axios from "axios";
 
+let req = new XMLHttpRequest();
+
+req.onreadystatechange = () => {
+  if (req.readyState == XMLHttpRequest.DONE) {
+    console.log(req.responseText);
+  }
+};
+
+
+
 function App() {
   const [tasks, setTasks] = useState([]);
   const createTask = async (title, taskDesc) => {
-    const response = await axios.post("https://my-json-server.typicode.com/canberk164/json-server/tasks", {
+    const response = await axios.post("https://api.jsonbin.io/v3/b/65d0f467dc74654018a62463/tasks", {
       title,
       taskDesc,
     });
@@ -17,8 +27,11 @@ function App() {
   };
 
   const fetchTasks = async () => {
-    const response = await axios.get("https://my-json-server.typicode.com/canberk164/json-server/tasks");
+    // const response = await axios.get("https://api.jsonbin.io/v3/b/65d0f467dc74654018a62463/tasks");
+    const response = req.open("GET", "https://api.jsonbin.io/v3/b/65d0f467dc74654018a62463/tasks", true);
+    req.setRequestHeader("$2a$10$uo31q.RVMZ5xJhte4DubqOHILYqKzvXnLrSkYf/yQDRyts3AXhV3e", "$2a$10$QBSbWbyer4VOfSkW7zXh4exSW7ICtyicKDRSTKuOGtpHwVP50tP4G");
     setTasks(response.data);
+    req.send();
   };
 
   useEffect(() => {
@@ -26,18 +39,23 @@ function App() {
   }, []);
 
   const deleteTaskById = async (id) => {
-    await axios.delete(`https://my-json-server.typicode.com/canberk164/json-server/tasks/${id}`);
+    // await axios.delete(`https://api.jsonbin.io/v3/b/65d0f467dc74654018a62463/tasks/${id}`);
+    await req.open("DELETE", `https://api.jsonbin.io/v3/b/65d0f467dc74654018a62463/tasks/${id}`, true);
+    req.setRequestHeader("$2a$10$uo31q.RVMZ5xJhte4DubqOHILYqKzvXnLrSkYf/yQDRyts3AXhV3e", "$2a$10$QBSbWbyer4VOfSkW7zXh4exSW7ICtyicKDRSTKuOGtpHwVP50tP4G");
+    
     const taskAfterDeleting = tasks.filter((task) => {
       return task.id !== id;
     });
     setTasks(taskAfterDeleting);
+    req.send();
   };
 
   const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
-    const response = await axios.put(`https://my-json-server.typicode.com/canberk164/json-server/tasks/${id}`, {
+    const response = await req.open("PUT", `https://api.jsonbin.io/v3/b/65d0f467dc74654018a62463/tasks/${id}`, {
       title: updatedTitle,
       taskDesc: updatedTaskDesc,
     });
+    req.setRequestHeader("$2a$10$uo31q.RVMZ5xJhte4DubqOHILYqKzvXnLrSkYf/yQDRyts3AXhV3e", "$2a$10$QBSbWbyer4VOfSkW7zXh4exSW7ICtyicKDRSTKuOGtpHwVP50tP4G");
     const updatedTask = tasks.map((task) => {
       if (task.id === id) {
         return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
@@ -45,6 +63,7 @@ function App() {
       return task;
     });
     setTasks(updatedTask);
+    req.send();
   };
 
   return (
