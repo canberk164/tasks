46e12450499fa15500e81b39a2bef362bbc92daa
canberk.tolunbuke <36010295+canberk164@users.noreply.github.com>
2024-02-20 18:57:44 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TaskCreate from "./components/TaskCreate";
 import TaskList from "./components/TaskList";
+import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const createTask = (title, taskDesc) => {
-    const createdTasks = [
-      ...tasks,
-      {
-        id: Math.round(Math.random() * 999999),
-        title,
-        taskDesc,
-      },
-    ];
+  const createTask = async (title, taskDesc) => {
+    const response = await axios.post("https://my-json-server.typicode.com/canberk164/tasksDB/tasks", {
+      title,
+      taskDesc,
+    });
+    const createdTasks = [...tasks, response.data];
 
     setTasks(createdTasks);
   };
-  const deleteTaskById = (id) => {
+
+  const fetchTasks = async () => {
+    const response = await axios.get("https://my-json-server.typicode.com/canberk164/tasksDB/tasks");
+    setTasks(response.data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const deleteTaskById = async (id) => {
+    await axios.delete(`https://my-json-server.typicode.com/canberk164/tasksDB/tasks/${id}`);
     const taskAfterDeleting = tasks.filter((task) => {
       return task.id !== id;
     });
     setTasks(taskAfterDeleting);
   };
 
-  const editTaskById = (id, updatedTitle, updatedTaskDesc) => {
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
+    const response = await axios.put(`https://my-json-server.typicode.com/canberk164/tasksDB/tasks/${id}`, {
+      title: updatedTitle,
+      taskDesc: updatedTaskDesc,
+    });
     const updatedTask = tasks.map((task) => {
       if (task.id === id) {
         return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
@@ -46,3 +59,5 @@ function App() {
     </div>
   );
 }
+
+export default App;
